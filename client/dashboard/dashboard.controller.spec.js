b81ffdacd9dc67c6036a1c5d00556e474b798192
Surabhi Sanjay Kumar <surabhis@CESCHN.COM>
2017-07-03 21:46:02 +0530

    created test files for dashboard

---
+++ client/dashboard/dashboard.controller.spec.js
@@ -0,0 +1,32 @@
+describe('SurveyTest Controller', function () {
+	var results = {};
+    var $controller;
+	var $location;
+	var $q;
+	var $rootscope;
+	var $scope;
+	var survey = {};
+	var ctrl = null;
+	
+	beforeEach(module('surveyApp'));
+    beforeEach(inject(function (_$controller_,_$location_, _$q_, _$rootScope_, _survey_) {
+		$controller = _$controller_;
+		$location = _$location_;
+		$scope = {};
+		$q = _$q_;
+		$rootscope = _$rootScope_;
+		survey = _survey_;
+	}));
+	it('should load survey results', function () {
+	    var response;
+	    spyOn(survey, 'getsurvey').and.callFake(function () {
+			var detail = $q.defer();
+			detail.resolve(results);
+			return detail.promise;
+		});
+        ctrl = $controller('dsController', {$scope: $scope});
+		$rootscope.$apply();
+	    survey.getsurvey();
+        expect(angular.equals(ctrl.results)).toBe(false);
+	});
+});
---
+++ client/dashboard/dashboard.service.spec.js
@@ -0,0 +1,24 @@
+describe('dasboard service', function() {
+	var productData ={"items":[{"survey_id":"1","topic":"Food Survey","created by":"Admin","description":"Do you need change in menu","Rating":"5"},{"survey_id":"2","topic":"Game Survey","created by":"Admin","description":"Need table tennis","Rating":"4"},{"survey_id":"3","topic":"Idea Survey","created by":"Admin","description":"Insurance idea is good or not","Rating":"5"},{"survey_id":"4","topic":"Holiday","created by":"Admin","description":"Planning for team holiday in some beautiful place","Rating":"5"},{"survey_id":"5","topic":"Festival","created by":"Admin","description":"celebration at office","Rating":"5"},{"survey_id":"6","topic":"A Survey","created by":"Admin","description":"abc","Rating":"4"},{"survey_id":"7","topic":"B Survey","created by":"Admin","description":"def","Rating":"4"},{"survey_id":"8","topic":"C Survey","created by":"Admin","description":"ghi","Rating":"4"},{"survey_id":"9","topic":"G Survey","created by":"Admin","description":"Need","Rating":"4"},{"survey_id":"10","topic":"10 Survey","created by":"Admin","description":"survey","Rating":"4"},{"survey_id":"11","topic":"11 Survey","created by":"Admin","description":"survey","Rating":"4"},{"survey_id":"12","topic":"G Survey","created by":"Admin","description":"Need","Rating":"4"},{"survey_id":"13","topic":"G Survey","created by":"Admin","description":"Need","Rating":"4"},{"survey_id":"14","topic":"G Survey","created by":"Admin","description":"Need","Rating":"4"},{"survey_id":"15","topic":"G Survey","created by":"Admin","description":"Need","Rating":"4"},{"survey_id":"16","topic":"G Survey","created by":"Admin","description":"Need","Rating":"4"}]};
+	var survey ={};
+	var $httpBackend;
+	beforeEach(module('surveyApp'));
+	beforeEach(inject(function(_survey_, _$httpBackend_){
+			survey=_survey_;
+		    $httpBackend = _$httpBackend_;
+		}));
+	
+	it('should return search survey data', function(){
+	var response;
+	var data;	
+		$httpBackend.when('GET', 'https://api.myjson.com/bins/r4prd')
+		.respond(200, productData);
+		 survey.getsurvey().then(
+				function (data) {
+				response = data;
+		        console.log(data);
+				});
+		$httpBackend.flush();
+		expect(angular.equals(data)).toBe(true);
+	});
+});
